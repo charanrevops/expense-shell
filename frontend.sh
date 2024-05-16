e30d0774d8bf3fe8f63e81079269eb7a060f938a
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-16 13:26:08 +0200

    Redirectors in frontend

--- frontend.sh
+++ frontend.sh
@@ -23,5 +23,4 @@ echo exit status - $?
 echo -e "\e[35mRestart Service\e[0m"
 systemctl enable nginx &>/tmp/expense.log
 systemctl restart nginx &>/tmp/expense.log
-echo exit status -
-echo -e "\e[35m$?\e[0m"
+echo exit status -$?
