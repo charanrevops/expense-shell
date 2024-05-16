5d543a117a90c8917722b25ba1b3a3cad5609954
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-16 13:03:46 +0200

    Redirectors in frontend

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,25 @@
 echo -e "\e[35mInstalling Nginx\e[0m"
-dnf install nginx -y &>tmp.expense.log
+dnf install nginx -y &>tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[35mCopy Expense Config File\e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>tmp.expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf &>tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[35mClean Old Content\e[0m"
-rm -rf /usr/share/nginx/html/* &>tmp.expense.log
+rm -rf /usr/share/nginx/html/* &>tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[35mDownload Frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>tmp.expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>tmp/expense.log
 cd /usr/share/nginx/html
 echo exit status - $?
 
 echo -e "\e[35mExtract Content\e[0m"
-unzip /tmp/frontend.zip &>tmp.expense.log
+unzip /tmp/frontend.zip &>tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[35mRestart Service\e[0m"
-systemctl enable nginx &>tmp.expense.log
-systemctl restart nginx &>tmp.expense.log
+systemctl enable nginx &>tmp/expense.log
+systemctl restart nginx &>tmp/expense.log
 echo exit status - $?
