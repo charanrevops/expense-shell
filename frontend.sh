562d7598b09058fae92283a2bfd5681d508fef02
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-16 13:14:05 +0200

    Redirectors in frontend

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 echo -e "\e[35mInstalling Nginx\e[0m"
 dnf install nginx -y &>>/tmp/expense.log
 echo exit status - $?
-echo exit status - $? &>>/tmp/expense.log
+echo "\e[35mexit status - $?\e[0m" &>>/tmp/expense.log
 
 echo -e "\e[35mCopy Expense Config File\e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
@@ -18,7 +18,6 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-fro
 echo exit status - $?
 
 cd /usr/share/nginx/html
-echo exit status - $?
 echo exit status - $? &>>/tmp/expense.log
 
 echo -e "\e[35mExtract Content\e[0m"
