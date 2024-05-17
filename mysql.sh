a893932b54667ffe4eab514b67654dc1307d3b43
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-17 15:48:51 +0200

    MySQL remove hardcoded password

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,12 @@
+echo -e "\e[35mInstalling mysql server\e[0m"
 dnf install mysql-server -y
+echo exit status -$?
+
+echo -e "\e[35mEnabling and starting mysql server\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo exit staus -$?
+
+echo -e "\e[35mInstallatioon and password setting of mysql server\e[0m"
+mysql_secure_installation --set-root-pass $1
+echo exit staus -$?
