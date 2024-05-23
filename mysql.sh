042daa03a391fe1389f7fc545bb0467b0272c394
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-23 09:24:42 +0200

    Adding common script for keeping code Dry

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,15 @@
 rm -f /tmp/expense.log
 
 
-echo -e "\e[35mInstalling mysql server\e[0m"
+HEADING Installing mysql server
 dnf install mysql-server -y
 echo exit status -$?
 
-echo -e "\e[35mEnabling and starting mysql server\e[0m"
+HEADING Enabling and starting mysql server
 systemctl enable mysqld
 systemctl start mysqld
 echo exit staus -$?
 
-echo -e "\e[35mInstallatioon and password setting of mysql server\e[0m"
+HEADING Installatioon and password setting of mysql server
 mysql_secure_installation --set-root-pass $1
 echo exit staus -$?
