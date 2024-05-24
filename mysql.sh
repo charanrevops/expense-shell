3472577bb1b5a9b338b424660e7359b183e4fbd0
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-24 14:36:06 +0200

    Validating input provided or not.

--- mysql.sh
+++ mysql.sh
@@ -5,8 +5,6 @@ rm -f /tmp/expense.log
 if [ -z "$1" ]; then
   echo Input MySQL password is missing
 
-
-
 HEADING Installing mysql server
 dnf install mysql-server -y
 echo exit status -$?
@@ -19,10 +17,3 @@ echo exit staus -$?
 HEADING Installatioon and password setting of mysql server
 mysql_secure_installation --set-root-pass $1
 echo exit staus -$?
-
-
-
-
-
-
-
