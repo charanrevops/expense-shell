deb7b16bcbc4802c7e44cef930236554139a39a9
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-24 15:38:44 +0200

    Exit Message print rather than code

--- mysql.sh
+++ mysql.sh
@@ -7,30 +7,26 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+STAT() {
+
+  if [ $? -eq 0 ];then
+    echo SUCCESS
+  else
+    echo FAILURE
+    exit 2
+  fi
+
 HEADING Installing mysql server
 dnf install mysql-server -y &>>/tmp/expense.log
-if [ $? -eq 0 ];then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 2
-fi
+STAT S?
+
 
 HEADING Enabling and starting mysql server
 systemctl enable mysqld &>>/tmp/expense.log
 systemctl start mysqld &>>/tmp/expense.log
-if [ $? -eq 0 ];then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 2
-fi
+STAT S?
+
 
 HEADING Installatioon and password setting of mysql server
 mysql_secure_installation --set-root-pass $1 &>>/tmp/expense.log
-if [ $? -eq 0 ];then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit 2
-fi
+STAT S?
