9275bdb3db9656a04006744ab20c37cf2a1a97cc
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-24 17:06:52 +0200

    Adding Backend.sh with standard options.

--- backend.sh
+++ backend.sh
@@ -61,7 +61,7 @@ STAT $?
 
 
 HEADING Load Schema
-mysql -h 172.31.32.211 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h localhost -uroot -pExpenseApp@1 < /app/schema/backend.sql
 STAT $?
 
 
